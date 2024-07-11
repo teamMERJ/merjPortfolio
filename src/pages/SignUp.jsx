e906773f6fdeb08cedb3d71f8ad4e673f7fba389
yanquehlina <c.maameyankeh@gmail.com>
2024-07-11 16:41:35 +0000

    add changes to signin page.

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { MailIcon, LockClosedIcon, UserIcon } from '@heroicons/react/outline';
-import { signUpImg } from '../assets';
+import React from "react";
+import { Link } from "react-router-dom";
+import { MailIcon, LockClosedIcon, UserIcon } from "@heroicons/react/outline";
+import { signUpImg } from "../assets";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="flex flex-col md:flex-row min-h-screen bg-gradient-to-r from-primary via-yellow-400 to-orange-400 text-white">
-      {/* Image Section */}
       <div className="hidden md:block md:w-1/2 bg-cover bg-center">
-        <img src={signUpImg} alt="Sign Up" className="h-full w-full object-cover" />
+        <img
+          src={signUpImg}
+          alt="Sign Up"
+          className="h-full w-full object-cover"
+        />
       </div>
 
-      {/* Form Section */}
       <div className="flex flex-col justify-center w-full md:w-1/2 p-8 bg-primary text-secondary">
-        <h1 className="text-3xl font-bold mb-6 text-center">Sign Up</h1>
-        <form className="w-full max-w-sm mx-auto">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Sign Up with merjPortfolio
+        </h1>
+
+        <form
+          className="w-full max-w-sm mx-auto text-primary"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div className="mb-4">
-            <label className="block mb-2" htmlFor="firstName">First Name</label>
+            <label className="block mb-2" htmlFor="firstName">
+              First Name
+            </label>
             <div className="relative">
               <UserIcon className="absolute left-3 top-3 h-5 w-5 text-gray-400" />
               <input
@@ -24,12 +44,25 @@ const SignUp = () => {
                 id="firstName"
                 placeholder="First Name"
                 className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-secondary focus:border-transparent"
-                required
+                {
+                  // register is from the react hook form and you resgister it with the name of the field. If the input is not optional, use required element to specify that that field should be filled. you can add min/mas length
+                  ...register("firstName", {
+                    required: "Your First Name is Required in this Field",
+                    maxLength: 15,
+                    minLength: 2,
+                  })
+                }
               />
+              {/*using 'logical and'(&&) to give  conditional error message */}
+              {errors.firstName && (
+                <p className="text-red-500">{errors.firstName.message}</p>
+              )}
             </div>
           </div>
           <div className="mb-4">
-            <label className="block mb-2" htmlFor="lastName">Last Name</label>
+            <label className="block mb-2" htmlFor="lastName">
+              Last Name
+            </label>
             <div className="relative">
               <UserIcon className="absolute left-3 top-3 h-5 w-5 text-gray-400" />
               <input
@@ -37,12 +70,20 @@ const SignUp = () => {
                 id="lastName"
                 placeholder="Last Name"
                 className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-secondary focus:border-transparent"
-                required
+                {...register("lastName", {
+                  required: "Your Last Name is Required in this Field",
+                  maxLength: 15,
+                })}
               />
+              {errors.lastName && (
+                <p className="text-red-500">{errors.lastName.message}</p>
+              )}
             </div>
           </div>
           <div className="mb-4">
-            <label className="block mb-2" htmlFor="otherNames">Other Names</label>
+            <label className="block mb-2" htmlFor="otherNames">
+              Other Names
+            </label>
             <div className="relative">
               <UserIcon className="absolute left-3 top-3 h-5 w-5 text-gray-400" />
               <input
@@ -50,24 +91,35 @@ const SignUp = () => {
                 id="otherNames"
                 placeholder="Other Names"
                 className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-secondary focus:border-transparent"
+                {...register("otherNames")}
               />
             </div>
           </div>
           <div className="mb-4">
-            <label className="block mb-2" htmlFor="email">Email</label>
+            <label className="block mb-2" htmlFor="email">
+              Email
+            </label>
             <div className="relative">
               <MailIcon className="absolute left-3 top-3 h-5 w-5 text-gray-400" />
               <input
                 type="email"
                 id="email"
-                placeholder="Email"
+                placeholder="name@gmail.com"
                 className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-secondary focus:border-transparent"
-                required
+                {...register("email", {
+                  required: "Your Email is Required in this Field",
+                  minLength: 2,
+                })}
               />
+              {errors.email && (
+                <p className="text-red-500">{errors.email.message}</p>
+              )}
             </div>
           </div>
           <div className="mb-4">
-            <label className="block mb-2" htmlFor="username">Username</label>
+            <label className="block mb-2" htmlFor="username">
+              Username
+            </label>
             <div className="relative">
               <UserIcon className="absolute left-3 top-3 h-5 w-5 text-gray-400" />
               <input
@@ -75,12 +127,21 @@ const SignUp = () => {
                 id="username"
                 placeholder="Username"
                 className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-secondary focus:border-transparent"
-                required
+                {...register("userName", {
+                  required: "Your Username is Required in this Field",
+                  maxLength: 15,
+                  minLength: 2,
+                })}
               />
+              {errors.userName && (
+                <p className="text-red-500">{errors.userName.message}</p>
+              )}
             </div>
           </div>
           <div className="mb-4">
-            <label className="block mb-2" htmlFor="password">Password</label>
+            <label className="block mb-2" htmlFor="password">
+              Password
+            </label>
             <div className="relative">
               <LockClosedIcon className="absolute left-3 top-3 h-5 w-5 text-gray-400" />
               <input
@@ -88,26 +149,53 @@ const SignUp = () => {
                 id="password"
                 placeholder="Password"
                 className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-secondary focus:border-transparent"
-                required
+                {...register("password", {
+                  required: "Your Password is Required in this Field",
+                  maxLength: 15,
+                  minLength: 2,
+                })}
               />
+              {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+              )}
             </div>
           </div>
           <div className="flex items-center mb-4">
-            <input type="checkbox" id="terms" className="mr-2" required />
+            <input
+              type="checkbox"
+              id="terms"
+              className="mr-2"
+              {...register("terms", {
+                required: "Please, Check this box",
+              })}
+            />
+            {errors.terms && (
+              <p className="text-red-500">{errors.terms.message}</p>
+            )}
             <label htmlFor="terms" className="text-sm">
-              I agree to the <Link to="/terms" className="text-white">Terms and Conditions</Link>
+              I agree to the{" "}
+              <Link to="/terms" className="text-white">
+                Terms and Conditions
+              </Link>
             </label>
           </div>
-          <button type="submit" className="bg-secondary text-primary px-4 py-2 rounded-full w-full">
+          <button
+            type="submit"
+            className="bg-secondary text-primary px-4 py-2 rounded-full w-full"
+          >
             Create Account
           </button>
         </form>
+
         <p className="mt-4 text-center">
-          Already have an account? <Link to="/signin" className="text-secondary">Login</Link>
+          Already have an account?{" "}
+          <Link to="/signin" className="text-secondary">
+            Login
+          </Link>
         </p>
       </div>
     </div>
   );
-}
+};
 
 export default SignUp;
