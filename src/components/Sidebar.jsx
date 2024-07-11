475fadff940bbdd70e10873982f26bfae4fdeced
yanquehlina <c.maameyankeh@gmail.com>
2024-07-11 13:23:11 +0000

    add changes to Overview page.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,25 @@
-import { Link } from 'react-router-dom';
-import Overview from '../pages/DashboardPage/Pages/Overview';
-import Skills from '../pages/DashboardPage/Pages/Skills';
-import Experiences from '../pages/DashboardPage/Pages/experiences';
-import Education from '../pages/DashboardPage/Pages/education';
-import Achivements from '../pages/DashboardPage/Pages/Achivements';
-import Projects from '../pages/DashboardPage/Pages/projects';
-import { Home } from 'lucide-react';
+import { Link } from "react-router-dom";
+import Overview from "../pages/DashboardPage/Pages/Overview";
+import Skills from "../pages/DashboardPage/Pages/Skills";
+import Experiences from "../pages/DashboardPage/Pages/experiences";
+import Education from "../pages/DashboardPage/Pages/education";
+import Achivements from "../pages/DashboardPage/Pages/Achivements";
+import Projects from "../pages/DashboardPage/Pages/projects";
+import { Home } from "lucide-react";
+import K from "../konstants";
 const Sidebar = () => {
   return (
-    <div className="h-screen shadow-2xl w-[250px] text-secondary flex flex-col bg-primary">
+    <div className="h-screen shadow-2xl w-[250px] text-secondary flex flex-col bg-primary px-8 py-12">
       <span className="text-2xl ">merjPorfolio</span>
+      <div className="flex flex-col gap-y-5 mt-5">
+        {K.NAVLINKS.map(({ icon, text, link }, index) => (
+          <Link>
+           
+          </Link>
+        ))}
+      </div>
 
-      <nav className="flex flex-col space-y-2 p-4">
+      {/* <nav className="flex flex-col space-y-2 p-4">
         <Link className="flex items-center space-x-2 hover:bg-gray-700 p-2 rounded">
         <Home className="w-6 h-6" />
         <span><Overview /></span>
@@ -37,9 +45,9 @@ const Sidebar = () => {
         <span><Projects /></span>
         </Link>
         
-    </nav>
+    </nav> */}
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
