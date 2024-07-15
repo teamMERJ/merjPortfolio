45f1d717841bb66282f96cc5b5712e458e54a1de
yanquehlina <c.maameyankeh@gmail.com>
2024-07-15 09:32:20 +0000

    add updates to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Landing from "./pages/Landing"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import Preview from "./pages/Preview"
+import AddSkill from "./pages/DashboardPage/Pages/Skills/AddSkill"
+import AddProject from "./pages/DashboardPage/Pages/Projects/AddProject"
+import AddExperience from "./pages/DashboardPage/Pages/Experience/AddExperience"
+import AddEducation from "./pages/DashboardPage/Pages/Education/AddEducation"
+import AddAchievement from "./pages/DashboardPage/Pages/Achievements/AddAchievements"
+import AddVolunteering from "./pages/DashboardPage/Pages/Volunteering/AddVolunteering"
+import AddProfile from "./pages/DashboardPage/Pages/Profile/AddProfile"
 import DashboardLayout from "./pages/DashboardPage/layouts"
 import Overview from "./pages/DashboardPage/Pages/Overview"
-import Skills from "./pages/DashboardPage/Pages/Skills"
-import Projects from "./pages/DashboardPage/Pages/projects"
-import Experiences from "./pages/DashboardPage/Pages/experiences"
-import Education from "./pages/DashboardPage/Pages/education"
-import Achivements from "./pages/DashboardPage/Pages/Achivements"
-import Socials from "./pages/DashboardPage/Pages/Socials"
-import SignUpPage from "./pages/SignUp"
+import Skills from "./pages/DashboardPage/Pages/Skills/Skills"
+import Landing from "./pages/Landing"
+import Preview from "./pages/Preview"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Projects from "./pages/DashboardPage/Pages/Projects/Projects"
+import Experience from "./pages/DashboardPage/Pages/Experience/Experience"
+import Education from "./pages/DashboardPage/Pages/Education/Education"
+import Profile from "./pages/DashboardPage/Pages/profile/Profile"
+import Achievements from "./pages/DashboardPage/Pages/Achievements/Achievements"
+import Volunteering from "./pages/DashboardPage/Pages/Volunteering/Volunteering"
+
 
 const App = () => {
   const router = createBrowserRouter([
@@ -40,28 +48,60 @@ const App = () => {
           element: <Overview />,
         },
         {
+          path: "profile",
+          element: <Profile />
+        },
+        {
+          path: "profile/add",
+          element: <AddProfile />
+        },
+        {
           path: "skills",
           element: <Skills />
+        },
+        {
+          path: "skills/add",
+          element: <AddSkill />
         },
         {
           path: "projects",
           element: <Projects />
         },
         {
-          path: "experiences",
-          element: <Experiences />
+          path: "project/add",
+          element: <AddProject/>
         },
         {
-          path: "educaion",
+          path: "experience",
+          element: <Experience />
+        },
+        {
+          path: "experience/add",
+          element: <AddExperience />
+        },
+        {
+          path: "education",
           element: <Education />
         },
         {
-          path: "achievements",
-          element: <Achivements />
+          path: "education/add",
+          element: <AddEducation />
         },
         {
-          path: "socials",
-          element: <Socials />
+          path: "achievements",
+          element: <Achievements />
+        },
+        {
+          path: "achievement/add",
+          element: <AddAchievement />
+        },
+        {
+          path: "voluntering",
+          element: <Volunteering />
+        },
+        {
+          path: "voluntering/add",
+          element: <AddVolunteering />
         }
       ],
     },
